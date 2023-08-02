b2235d5bd7c7e765e21f4d636f8572455b665655
mateushilariodias <mateusilariodias@gmail.com>
2023-08-01 22:07:31 -0300

    first commit

---
+++ convert-string-to-number.mjs
@@ -0,0 +1,41 @@
+// Definindo a constante 'um' com o valor string '1'
+const um = '1';
+
+// Imprimindo o valor de 'um' no console
+console.log(um); 
+
+// Convertendo a string 'um' para número usando o operador '+'
+const mais = +um;
+
+// Imprimindo o valor de 'mais' no console
+console.log(mais); 
+
+// Convertendo a string 'um' para número ao multiplicá-la por 1
+const vezes = 1*um;
+
+// Imprimindo o valor de 'vezes' no console
+console.log(vezes);
+
+// Convertendo a string 'um' para número usando o operador bit a bit '~~'
+const doisTil = ~~um;
+
+// Imprimindo o valor de 'doisTil' no console
+console.log(doisTil);
+
+// Convertendo a string 'um' para número usando a função 'Number()'
+const num = Number(um);
+
+// Imprimindo o valor de 'num' no console
+console.log(num);
+
+// Convertendo a string 'um' para número usando a função 'parseInt()'
+const int = parseInt(um);
+
+// Imprimindo o valor de 'int' no console
+console.log(int);
+
+// Convertendo a string 'um' para número usando a função 'parseFloat()'
+const float = parseFloat(um);
+
+// Imprimindo o valor de 'float' no console
+console.log(float);
