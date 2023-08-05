a74384c3c0916c4e44dccc72dd52df8a9ee04b23
mateushilariodias <mateusilariodias@gmail.com>
2023-08-05 15:46:53 -0300

    change file array-methods.mjs

--- array-methods.mjs
+++ array-methods.mjs
@@ -26,4 +26,22 @@ console.log(resultOne); // Output: undefined (pois "Motorola" não existe no arr
 const result = cellphones.find(cellphone => cellphone === "Samsung");
 console.log(result); // Output: "Samsung" (pois "Samsung" existe no array)
 
+// .map() cria um novo array transformando cada elemento do array original.
+// Neste caso, estamos criando um novo array com o valor 'White' para cada elemento das cores primárias.
+const primaryColors = ['Red', 'Yellow', 'Blue'].map(element => 'White');
+console.log(primaryColors);
 
+// .filter() cria um novo array com todos os elementos que passam no teste implementado pela função fornecida.
+// Neste caso, estamos filtrando apenas a cor 'Purple' das cores secundárias.
+const secondaryColors = ['Green', 'Purple', 'Orange', 'Purple'].filter(element => element === 'Purple');
+console.log(secondaryColors);
+
+// .some() testa se pelo menos um elemento no array passa no teste implementado pela função fornecida.
+// Aqui, estamos verificando se a cor 'Yellow' existe nas cores terciárias.
+const tertiaryColors = ['Yellow', 'Red', 'Black'].some(element => element === 'Yellow');
+console.log(tertiaryColors);
+
+// .every() testa se todos os elementos no array passam no teste implementado pela função fornecida.
+// Neste caso, estamos verificando se todas as cores neutras são iguais a 'Brown'.
+const neutralColors = ['White', 'Grey', 'Brown'].every(element => element === 'Brown');
+console.log(neutralColors);
